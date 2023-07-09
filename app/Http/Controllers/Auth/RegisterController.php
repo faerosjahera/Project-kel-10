cfd48b2bbd4fa1a86e7745257a47691649c4a753
faerosjahera <ifairuzahira@gmail.com>
2023-07-09 18:35:06 +0700

    update regis

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -51,6 +51,8 @@ class RegisterController extends Controller
     {
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
+            'tgl_lhr' => ['required', 'string','date','max:255'],
+            'telp' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
@@ -66,6 +68,8 @@ class RegisterController extends Controller
     {
         return User::create([
             'name' => $data['name'],
+            'tgl_lhr' => $data['tgl_lhr'],
+            'telp' => $data['telp'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
